f94ab4a477d7ed664a73509ed4ca49149ef1a32b
dolez-ma <31374761+dolez-ma@users.noreply.github.com>
2018-06-10 04:30:12 +0200

    WIP framework

---
+++ app/Configuration/Beast.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Boudha
+ * Date: 10/06/2018
+ * Time: 01:09
+ */
+
+namespace Configuration;
+
+use BeastPHP\Framework\Interfaces\Configuration;
+
+class Beast implements Configuration
+{
+
+    public function getSortOrder()
+    {
+        // TODO: Implement getSortOrder() method.
+    }
+
+    public function getValues()
+    {
+        return [];
+    }
+}
